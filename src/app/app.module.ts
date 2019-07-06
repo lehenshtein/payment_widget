ceef281485eaedd165a3df469f61ca3dceada108
lehenshtein <lehenshtein@gmail.com>
2019-07-06 21:18:32 +0300

    ready to go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {MaterialModule} from './shared/material/material.module';
+import { WidgetComponent } from './widget/widget.component';
+import { FormComponent } from './widget/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WidgetComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
